ff20ae31bbc2da663930607c864b8de8213f55a7
Jonathan Price <jon.mark.price@gmail.com>
2015-03-25 15:26:19 -0500

    basic db connection setup

--- 315/326/main.php
+++ 315/326/main.php
@@ -1,10 +1,12 @@
 <?php
-  require_once(../../../cs315/db_login.php);
+  require_once("../../../cs315/db_login.php");
 
   $db = new PDO(
     "mysql:host=$db_hostname;dbname=jmp3748;charset=utf8",
     $db_username, $db_password,
     array(PDO::ATTR_EMULATE_PREPARES => false,
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+  $db->query('select * from test;');
 
 ?>
