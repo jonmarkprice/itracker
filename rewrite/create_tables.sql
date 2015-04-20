31b3acd89f7d20ef51495e2584e099a590d173f9
Jonathan Price <jon.mark.price@gmail.com>
2015-04-20 03:15:54 -0500

    initial inventory, based on display_info

--- rewrite/create_tables.sql
+++ rewrite/create_tables.sql
@@ -2,13 +2,14 @@ use jmp3748;
 
 drop table if exists item;
 create table item (
-  id int primary key,
-  item_name varchar(255) not null,
+  id int
+  owner varchar(255) references user,
+  name varchar(255) not null,
   unit enum('Each','6-pack') not null,
   description varchar(255),
-  item_type varchar(255),
   quantity int,
-  in_date date
+  in_date date,
+  primary key (id, owner)
 );
 
 drop table if exists user;
