2831804215d44cc32106aee09df80c77d525b903
Jonathan Price <jon.mark.price@gmail.com>
2015-04-20 12:16:44 -0500

    ...

--- rewrite/lib/create_tables.sql
+++ rewrite/lib/create_tables.sql
@@ -8,21 +8,15 @@ create table item (
   unit enum('Each','6-pack') not null,
   description varchar(255),
   quantity int,
-  in_date date,
   primary key (id, owner),
-  foreign key (owner) references user(username) on delete cascade
+  foreign key (owner) references user(username)
 );
 
-drop table if exists item;
-create table item (
-  id int,
-  owner varchar(255),
-  name varchar(255) not null,
-  unit enum('Each','6-pack') not null,
-  description varchar(255),
-  quantity int,
-  in_date date,
-  primary key (id, owner),
-  foreign key (owner) references user(username) on delete cascade
+drop table if exists user;
+create table user (
+  username varchar(255) primary key,
+  firstname varchar(255),
+  lastname varchar(255),
+  password_hash varchar(255) not null,
+  email varchar(255)
 );
-
